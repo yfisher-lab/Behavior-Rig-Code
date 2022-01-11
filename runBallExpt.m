c74197553dd6f3d558aa01d3fc49e62afdb6fe23
Yvette <yfisher@berkeley.edu>
2022-01-11 09:58:04 -0800

    Added Phidget socket script and runExpt initial outline
    wrote first round of Matlab based code that calls FicTrac and socket client.  Still needs panels and aquisition added

---
+++ runBallExpt.m
@@ -0,0 +1,28 @@
+%runBallExpt
+%
+%
+%
+% Script for running fly on ball expeirment using FicTrac and Reiser LED
+% panels VR system
+%
+% Yvette Fisher 12/2021
+
+
+% TODO - Configure panels, for closed loop mode and set up which pattern to use
+% and set up external tiggering if you want things to
+% start with a trigger, or just have the pattern start if that is
+% easier....
+
+% Start FicTrac in background from current experiment directory (config file must be in directory)
+FT_PATH = 'C:\Users\fisherlab\Documents\GitHub\ficTrac\';
+FT_EXE_FILENAME = 'fictrac.exe';
+cmdStr = ['cd "', FT_PATH, '" ', '& start ', FT_PATH, FT_EXE_FILENAME];
+system(cmdStr);
+pause(4);
+
+% Call socket_client_360 to open socket connection from fictrac to Phiget22 device
+Socket_PATH = 'C:\Users\fisherlab\Documents\GitHub\Behavior-Rig-Code\';
+SOCKET_SCRIPT_NAME = 'socket_client_360.py';
+%cmdstring = ['cd "' Socket_PATH '" & python ' SOCKET_SCRIPT_NAME ' &'];
+cmdstring = ['cd "' Socket_PATH '" & py ' SOCKET_SCRIPT_NAME ' &'];
+[status] = system(cmdstring, '-echo');
